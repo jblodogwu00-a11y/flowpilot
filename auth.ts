41672122d2bb2783b083dda9d731037365efcafa
James Brown <jblodogwu00@gmail.com>
2026-09-20 20:11:25 +0100

    Add trustHost setting to fix invalid code verifier error

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
