bde3bbd215ebb33f1d36fdcacf82e6b85a1bb9b0
James Brown <jblodogwu00@gmail.com>
2026-09-21 21:57:57 +0100

    Simplify auth config to fix server error

--- auth.ts
+++ auth.ts
@@ -8,11 +8,6 @@ const prisma = new PrismaClient();
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   trustHost: true,
-  callbacks: {
-    async redirect({ baseUrl }) {
-      return `${baseUrl}/dashboard`;
-    },
-  },
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
